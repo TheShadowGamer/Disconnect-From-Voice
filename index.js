aeef92fe2ce38ea666aa5c1a5b8e10faf4d72017
Anonymous <28690521+TheShadowGamer@users.noreply.github.com>
2020-11-10 17:58:35 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Plugin } = require('powercord/entities')
+const { getModule } = require("powercord/webpack")
+const { selectVoiceChannel } = getModule(["selectVoiceChannel"], false)
+module.exports = class DisconnectVoice extends Plugin {
+  startPlugin () {
+    document.body.addEventListener("keyup", this.keyup)
+  }
+  pluginWillUnload () {
+    document.body.removeEventListener("keyup", this.keyup)
+  }
+  async keyup(event) {
+    if(event.key == "F4") {
+      selectVoiceChannel(null)
+    }
+  }
+}
