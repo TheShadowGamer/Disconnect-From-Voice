2386a4ad7812c0e8888fd99e556d028139866afe
Anonymous <28690521+TheShadowGamer@users.noreply.github.com>
2020-11-11 18:44:09 -0500

    Ability To Change Keybind

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,14 +1,22 @@
 const { React } = require('powercord/webpack');
-const { FormTitle } = require('powercord/components');
-const { SwitchItem, ButtonItem, Category, KeybindRecorder } = require('powercord/components/settings');
-const { Button } = require('powercord/components');
+const KeybindRecorder = require('./KeybindRecorder');
 
 module.exports = class Settings extends React.Component {
     render () {
+        const { getSetting, updateSetting } = this.props;
+        
         return (
             <div>
-                <FormTitle>Edit Keybinds</FormTitle>
-                <KeybindRecorder></KeybindRecorder>
+                <KeybindRecorder value={getSetting("disconnectBind")} 
+                onChange={(e) => {
+                    this.setState({value: e})
+                    updateSetting("disconnectBind", e)
+                }}
+                onReset={() => {
+                    this.setState({value: "F3"})
+                    updateSetting("disconnectBind", "F3")
+                }}
+                >Disconnect From Voice Keybind</KeybindRecorder>
             </div>
         )
     }
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const powercord = require('powercord/');
 const { Plugin } = require('powercord/entities')
 const { getModule } = require("powercord/webpack")
 const { selectVoiceChannel } = getModule(["selectVoiceChannel"], false)
-const Settings = require('./components/Settings.jsx');
+const Settings = require('./components/Settings');
+let _this
 module.exports = class DisconnectVoice extends Plugin {
   startPlugin () {
+    _this = this;
+    if(!_this.settings.get("disconnectBind")) _this.settings.set("disconnectBind", "F3")
     powercord.api.settings.registerSettings("disconnect-voice", {
-      label: "Disconnect-From-Voice",
+      label: "Disconnect From Voice",
       category: this.entityID,
       render: Settings
     })
-    document.body.addEventListener("keyup", this.keyup)
+    document.body.addEventListener("keyup", this.keydown)
   }
   pluginWillUnload () {
-    document.body.removeEventListener("keyup", this.keyup)
+    document.body.removeEventListener("keyup", this.keydown)
     powercord.api.settings.unregisterSettings("disconnect-voice")
   }
-  async keyup(event) {
-    if(event.key == "F3") {
+  async keydown(event) {
+    if(event.key.toUpperCase() === _this.settings.get("disconnectBind")) {
       selectVoiceChannel(null)
     }
   }
